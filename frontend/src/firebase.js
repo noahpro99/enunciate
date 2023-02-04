172b76cd48c26d3c63850c52295d433ab429a92d
Noah Provenzano <noahpro@gmail.com>
2023-02-04 14:52:21 -0500

    firebase key fix

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -5,13 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFLmM6uJhwdMq9LQgsC2ZZBOLV6yLfGYw",
-    authDomain: "brainweb-d40f2.firebaseapp.com",
-    projectId: "brainweb-d40f2",
-    storageBucket: "brainweb-d40f2.appspot.com",
-    messagingSenderId: "547767204969",
-    appId: "1:547767204969:web:de7318d2fd1db16e7636f8",
-    measurementId: "G-NL0H5J38SL"
+    apiKey: "AIzaSyDxzHPDWuXX8lQDgHBY3VScmc2zmFuuCyQ",
+    authDomain: "enunciate-48cde.firebaseapp.com",
+    projectId: "enunciate-48cde",
+    storageBucket: "enunciate-48cde.appspot.com",
+    messagingSenderId: "490550703572",
+    appId: "1:490550703572:web:bea52a84e99aa65426d241"
 };
 
 export const app = initializeApp(firebaseConfig);
